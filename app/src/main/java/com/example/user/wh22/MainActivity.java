f3cb61606f5704c47dec64794ef678697e59d7f0
wsadzxckenny <s5309pp@gmail.com>
2020-04-08 22:46:57 +0800

    logic

--- app/src/main/java/com/example/user/wh22/MainActivity.java
+++ app/src/main/java/com/example/user/wh22/MainActivity.java
@@ -13,10 +13,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    String ans ="1234";
     public void myfun(View btnView){
 
         EditText et=(EditText) findViewById(R.id.editText);
         TextView tv=(TextView) findViewById(R.id.textView);
-        tv.setText(et.getText());
+        char[] guess=et.getText().toString().toCharArray();
+        char[] ansArray= ans.toCharArray();
+        int nA =0;
+        int nB =0;
+        for(int i=0; i < guess.length; i++) {
+            for (int j = 0; j < ansArray.length; j++) {
+                if ( guess[i]==ansArray[j])
+                {
+                    if ( i==j)
+                        nA++;
+                    else
+                        nB++;
+                }
+            }
+        }
+        String ret =nA +"A"+nB+"B";
+        tv.setText(ret);
     }
+
 }
